8321b4e62adf7ab528d2e108539c0d76eb4014a9
bastiendev <bastienfontaine.abc@laposte.net>
2016-03-08 16:38:13 +0100

    theme final 2

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Logo Nav by Start Bootstrap</h1>
+                <h1>Logo Nav by Start Bootstrapp</h1>
                 <p>Note: You may need to adjust some CSS based on the size of your logo. The default logo size is 150x50 pixels.</p>
             </div>
         </div>
