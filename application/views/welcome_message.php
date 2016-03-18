c8f603e701d1c79891afac89ee1064faa7692a95
bastiendev <bastienfontaine.abc@laposte.net>
2016-03-18 08:37:27 +0100

    testttttt

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>PROJET APPLI-CR</h1>
+                <h1>PROJET APPLI-CRr</h1>
                 <p>L'application de gestion des comptes-rendus de visite va vous servir à conserver un suivi informatisé des rapports produits à chaque visite. </br>
 					</br>
 					Le renseignement précis des éléments de compte-rendu vous permettra d'établir un suivi fidèle des différentes rencontres realisées avec les </br>
